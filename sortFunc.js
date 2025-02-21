1355b91998005b78427efdad6b0d36ac0a014748
ylee <leeyn.shun@gmail.com>
2025-02-21 20:55:56 +0200

    Feature: Sort added.

---
+++ sortFunc.js
@@ -0,0 +1,79 @@
+let sortCriteria = 'Name';
+let isAscd = true;
+
+export function sortTable(
+    heroes,
+    reqCriteria,
+    heroTable,
+    insertHeaders,
+    insertHeroEntries
+){
+    if (sortCriteria === reqCriteria){
+        isAscd = !isAscd;
+    } else {
+        sortCriteria = reqCriteria;
+        isAscd = true;
+    };
+    const reqOrder = (isAscd) ? 1 : -1;
+
+    let sortFunc = sortName;
+    if (sortCriteria === 'Full Name') sortFunc = sortFullName;
+    if (sortCriteria === 'Powerstats') sortFunc = sortPowerStats;
+    if (sortCriteria === 'Race') sortFunc = sortRace;
+    if (sortCriteria === 'Gender') sortFunc = sortGender;
+    if (sortCriteria === 'Height') sortFunc = sortHeight;
+    if (sortCriteria === 'Weight') sortFunc = sortWeight;
+    if (sortCriteria === 'Place Of \nBirth') sortFunc = sortPOB;
+    if (sortCriteria === 'Alignment') sortFunc = sortAlignment;
+
+    heroes.sort((a, b) =>sortFunc(a,b,reqOrder));
+    heroTable.innerHTML = '';
+    insertHeaders(heroes);
+    heroes.forEach(insertHeroEntries);
+};
+
+export function sortAB(a,b, reqOrder, invalidCase){
+    if (a === invalidCase) return 1;
+    if (b === invalidCase) return -1;
+    if (a < b) return -reqOrder;
+    if (a > b) return reqOrder;
+    return 0;
+};
+
+export function sortName(a, b, reqOrder) {
+    return sortAB(a.name, b.name, reqOrder, '');
+};
+
+export function sortFullName(a, b, reqOrder) {
+    return sortAB(a.biography.fullName , b.biography.fullName , reqOrder, '');
+};
+
+export function sortPowerStats(a, b, reqOrder) {
+    const aPowerStats = Object.values(a.powerstats).reduce((sum, value) => sum + value, 0);
+    const bPowerStats = Object.values(b.powerstats).reduce((sum, value) => sum + value, 0);
+    return sortAB(aPowerStats, bPowerStats, reqOrder, 'null');
+};
+
+export function sortRace(a, b, reqOrder) {
+    return sortAB(a.appearance.race, b.appearance.race, reqOrder, null);
+};
+
+export function sortGender(a, b, reqOrder) {
+    return sortAB(a.appearance.gender, b.appearance.gender, reqOrder, '-');
+};
+
+export function sortHeight(a, b, reqOrder) {
+    return sortAB(a.appearance.height, b.appearance.height, reqOrder, null);
+};
+
+export function sortWeight(a, b, reqOrder) {
+    return sortAB(a.appearance.weight, b.appearance.weight, reqOrder, null);
+};
+
+export function sortPOB(a, b, reqOrder) {
+    return sortAB(a.biography.placeOfBirth, b.biography.placeOfBirth, reqOrder, '-');
+};
+
+export function sortAlignment(a, b, reqOrder) {
+    return sortAB(a.biography.alignment, b.biography.alignment, reqOrder, '-');
+};
--- sortable.js
+++ sortable.js
@@ -1,14 +1,49 @@
 // display data in a table. DONE
-// sort table by any column
+// sort table by any column. DONE
 // use a select input to chose page size
 // interactive search
 // optimize
 // specify field that search is apply to
 // custom searhc operators
-// detail ciew
+// detail view
 // css
 // url query to determine search conditions
-function getTable(heroes){
+import { sortTable } from "./sortFunc.js";
+
+const heroTable = document.createElement('table');
+const pwrStatsAbrv = {
+    'intelligence': 'INT',
+    'strength': 'STR',
+    'speed': 'SPD',
+    'durability': 'DUR',
+    'power': 'PWR',
+    'combat': 'COM'
+}
+const headers = [
+    '',
+    'Name',
+    'Full Name',
+    'Powerstats',
+    'Race',
+    'Gender',
+    'Height',
+    'Weight',
+    `Place Of \nBirth`,
+    'Alignment'
+];
+
+export function init(heroes){
+    heroes[135].appearance.height = ["5'5", "165 cm"]
+    heroes.forEach((hero)=>{
+        const heightParam = hero.appearance.height[1].split(' ');
+        hero.appearance.height = Number(heightParam[0])
+        if (heightParam[1] === 'meters') hero.appearance.height *= 10;
+
+        const weightParam = hero.appearance.weight[1].split(' ');
+        hero.appearance.weight = Number(weightParam[0].replaceAll(',',''));
+        if (weightParam[1] === 'tons') hero.appearance.weight *= 1000;
+    });
+
     const optionDiv = document.createElement('div');
     optionDiv.className = 'options';
     document.body.appendChild(optionDiv);
@@ -29,81 +64,73 @@ function getTable(heroes){
     });
     selectDiv.appendChild(selectInput);
     optionDiv.appendChild(selectDiv);
-    
-    const heroTable = document.createElement('table');
-    const headerRow = heroTable.insertRow();
-    const headers = [
-        'Icon',
-        'Name',
-        'Full Name',
-        'Powerstats',
-        'Race',
-        'Gender',
-        'Height',
-        'Weight',
-        `Place Of \nBirth`,
-        'Alignment'
-    ];
 
-    const pwrStatsAbrv = {
-        'intelligence': 'INT',
-        'strength': 'STR',
-        'speed': 'SPD',
-        'durability': 'DUR',
-        'power': 'PWR',
-        'combat': 'COM'
-    }
+    insertHeaders(heroes);
+    heroes.forEach((hero) => insertHeroEntries(hero, heroTable));
+
+    document.body.appendChild(heroTable);
+};
+
+
+function insertHeaders(heroes){
+    const headerRow = heroTable.insertRow();
 
     headers.forEach(headerTitle => {
         const th = document.createElement('th');
         th.textContent = headerTitle;
         headerRow.appendChild(th);
-    });
 
-    heroes.forEach((hero) => {
-        const curRow = heroTable.insertRow();
-
-        const iconCell = curRow.insertCell();
-        const img = document.createElement('img');
-        img.src = hero.images.xs;
-        img.alt = hero.name;
-        iconCell.appendChild(img);
-
-        const nameCell = curRow.insertCell();
-        nameCell.textContent = hero.name;
-    
-        const fullNameCell = curRow.insertCell();
-        fullNameCell.textContent = hero.biography.fullName;
-
-        const powerStatsCell = curRow.insertCell();
-        let strContainer = '';
-        Object.entries(hero.powerstats).forEach(([key, value]) => {
-            strContainer += `${pwrStatsAbrv[key]}: ${value}\n`;
+        th.addEventListener('click', ()=>{
+            sortTable(
+                heroes,
+                headerTitle,
+                heroTable,
+                insertHeaders,
+                insertHeroEntries
+            );
         });
-        powerStatsCell.textContent = strContainer;
-
-        const raceCell = curRow.insertCell();
-        raceCell.textContent = hero.appearance.race;
-
-        const genderCell = curRow.insertCell();
-        genderCell.textContent = hero.appearance.gender;
-
-        const heightCell = curRow.insertCell();
-        heightCell.textContent = hero.appearance.height.join('\n');
-
-        const weightCell = curRow.insertCell();
-        weightCell.textContent = hero.appearance.weight.join('\n');
-
-        const placeOfBirthCell = curRow.insertCell();
-        placeOfBirthCell.textContent = hero.biography.placeOfBirth;
-
-        const alignmentCell = curRow.insertCell();
-        alignmentCell.textContent = hero.biography.alignment;
     });
-
-    document.body.appendChild(heroTable);
 }
 
-export function sortable(heroes){
-    getTable(heroes);
-};
+function insertHeroEntries(hero){
+    const curRow = heroTable.insertRow();
+
+    const iconCell = curRow.insertCell();
+    const img = document.createElement('img');
+    img.src = hero.images.xs;
+    img.alt = hero.name;
+    iconCell.appendChild(img);
+
+    const nameCell = curRow.insertCell();
+    nameCell.textContent = hero.name;
+
+    const fullNameCell = curRow.insertCell();
+    fullNameCell.textContent = hero.biography.fullName;
+
+    const powerStatsCell = curRow.insertCell();
+    let strContainer = '';
+    Object.entries(hero.powerstats).forEach(([key, value]) => {
+        strContainer += `${pwrStatsAbrv[key]}: ${value}\n`;
+    });
+    powerStatsCell.textContent = strContainer;
+
+    const raceCell = curRow.insertCell();
+    raceCell.textContent = hero.appearance.race;
+
+    const genderCell = curRow.insertCell();
+    genderCell.textContent = hero.appearance.gender;
+
+    const heightCell = curRow.insertCell();
+    heightCell.className = 'height'
+    heightCell.textContent = hero.appearance.height;
+
+    const weightCell = curRow.insertCell();
+    weightCell.className = 'weight'
+    weightCell.textContent = hero.appearance.weight;
+
+    const placeOfBirthCell = curRow.insertCell();
+    placeOfBirthCell.textContent = hero.biography.placeOfBirth;
+
+    const alignmentCell = curRow.insertCell();
+    alignmentCell.textContent = hero.biography.alignment;
+}
